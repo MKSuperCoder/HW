316bb4f96a0b4a4a1e3d7a0eb12b20c82e110181
Marcus Swa <89757146+MKSuperCoder@users.noreply.github.com>
2025-02-12 21:51:47 -0500

    Initial Commit

--- hw/ChallengeProject/Starter/Program.cs
+++ hw/ChallengeProject/Starter/Program.cs
@@ -1,7 +1,7 @@
 using System;
 using System.IO;
 
-// the ourAnimals array will store the following: 
+// the ourAnimals array will store the following: ss
 string animalSpecies = "";
 string animalID = "";
 string animalAge = "";
