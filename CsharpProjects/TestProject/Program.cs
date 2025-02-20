1d8c4772b62062487dffe1c040ad63250d44e145
Marcus Swa <89757146+MKSuperCoder@users.noreply.github.com>
2025-02-19 20:14:09 -0500

    Unit 6

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -20,10 +20,18 @@ string[] pettingZoo =
     - Print the animal groups */
 
 #endregion
-RandomizeAnimals();
-string[,] group = AssignGroup();
-Console.WriteLine("School A");
-//PrintGroup(group)
+
+PlanSchoolVisit("School A");
+PlanSchoolVisit("School B", 3);
+PlanSchoolVisit("School C", 2);
+
+void PlanSchoolVisit(string schoolName, int groups = 6)
+{
+    RandomizeAnimals();
+    string[,] group = AssignGroup(groups);
+    Console.WriteLine(schoolName);
+    PrintGroup(group);
+}
 
 void RandomizeAnimals()
 {
@@ -51,6 +59,18 @@ string[,] AssignGroup(int groups = 6)
         }
     }
     return result;
+}
+void PrintGroup(string[,] group)
+{
+    for (int i = 0; i < group.GetLength(0); i++)
+    {
+        Console.Write($"Group {i + 1}: ");
+        for (int j = 0; j < group.GetLength(1); j++)
+        {
+            Console.Write($"{group[i,j]} ");
+        }
+        Console.WriteLine();
+    }
 }
 #region Test RandomizeAnimals() method
 /*foreach (string animal in pettingZoo)
